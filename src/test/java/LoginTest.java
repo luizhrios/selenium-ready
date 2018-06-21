a23bb63963c645351b408faf7428e55955ef2c84
Luiz Henrique Rios <luizhrios@gmail.com>
2018-06-20 22:35:36 -0300

    Selenium-Ready

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,42 @@
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest
+{
+
+	static WebDriver driver;
+
+	String url = "https://github.com/login";
+
+	@Before
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver", "chromedriver\\win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(url);
+	}
+
+	@Test
+	public void test()
+	{
+		WebElement element = driver.findElement(By.name("login"));
+		element.sendKeys("login");
+		element = driver.findElement(By.name("password"));
+		element.sendKeys("pass*");
+		// driver.findElement(By.name("commit")).click();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws InterruptedException
+	{
+		Thread.sleep(5000);
+		driver.close();
+		driver.quit();
+	}
+
+}
